6ec45e832178fb5e6fde893ee1322eff39ce409c
Boggie217 <94899315+Boggie217@users.noreply.github.com>
2024-04-16 15:20:55 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,1 +1,48 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer-container">
+      <div className="container">
+        <div className="row align-items-center"> {/* Added class for centering */}
+          <div className="col text-center"> {/* Added class for centering content */}
+            <h5>Follow me!</h5>
+            <ul className="list-inline justify-content-center"> {/* Added class for centering icons */}
+              <li className="list-inline-item">
+                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
