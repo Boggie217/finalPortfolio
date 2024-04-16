0e55583bb477655dd32080d8610e61dff979a873
Boggie217 <94899315+Boggie217@users.noreply.github.com>
2024-04-16 15:21:23 +0100

    Update profilesection.js

--- src/components/profilesection.js
+++ src/components/profilesection.js
@@ -1,1 +1,24 @@
+import React from 'react';
 
+
+const ProfileSection = () => {
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-md-6">
+          
+        </div>
+        <div className="col-md-6">
+          <h1 className="display-4">Welcome to My Portfolio</h1>
+          <p className="lead">
+            Hello there! I'm [Brad], a passionate [web developer and occasional linguist ] based
+            in [Northern England]. Explore my work and let's collaborate on
+            exciting projects.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileSection;
