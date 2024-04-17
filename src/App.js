a25b45c95a95b6374155e0e3e9832cb196731753
Boggie217 <brad98ogden@gmail.com>
2024-04-17 16:12:33 +0100

    another commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
